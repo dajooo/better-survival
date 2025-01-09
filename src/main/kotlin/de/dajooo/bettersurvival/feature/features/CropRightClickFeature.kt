7fb926e1f9181a07b89e96ab897896dc70c76db2
dajooo <contact@dajooo.de>
2025-01-09 14:56:37 +0100

    fix: simulate break effect on crop right click

--- src/main/kotlin/de/dajooo/bettersurvival/feature/features/CropRightClickFeature.kt
+++ src/main/kotlin/de/dajooo/bettersurvival/feature/features/CropRightClickFeature.kt
@@ -7,11 +7,17 @@ import de.dajooo.kaper.extensions.isTagged
 import de.dajooo.kaper.extensions.not
 import de.dajooo.kaper.extensions.tagKeyFor
 import kotlinx.serialization.Serializable
+import net.kyori.adventure.key.Key
+import net.kyori.adventure.sound.Sound
 import org.bukkit.Material
+import org.bukkit.Particle
 import org.bukkit.block.data.Ageable
 import org.bukkit.event.EventHandler
 import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.util.Vector
+import kotlin.math.cos
+import kotlin.math.sin
 
 class CropRightClickFeature: AbstractFeature<CropRightClickFeature.Config>() {
     @Serializable
@@ -50,6 +56,26 @@ class CropRightClickFeature: AbstractFeature<CropRightClickFeature.Config>() {
                 event.player.world.dropItemNaturally(block.location, it)
             }
         }
+
+        val loc = block.location
+        event.player.spawnParticle(
+            Particle.BLOCK,
+            loc.clone().add(0.5, 0.25, 0.5),
+            30,
+            0.4, 0.4, 0.4,
+            0.0,
+            block.blockData
+        )
+
+        event.player.playSound(
+            Sound.sound(
+                Key.key("block.crop.break"),
+                Sound.Source.BLOCK,
+                1.0f,
+                1.0f
+            )
+        )
+
         block.blockData = crop
         event.isCancelled = true
     }
